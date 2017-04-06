f4b9efc0a5b514164214a01e135cfbe80fe45a66
Bogdan Popov <popovbodya@gmail.com>
2017-04-06 17:09:55 +0300

    Money tracker added

---
+++ tracker/MoneyTracker/app/src/main/java/ru/popovbodya/moneytracker/MainActivity.java
@@ -0,0 +1,53 @@
+package ru.popovbodya.moneytracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+
+public class MainActivity extends Activity {
+
+    private static class Item {
+        private final String name;
+        private final int price;
+
+        public Item(String name, int price) {
+            this.name = name;
+            this.price = price;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        final EditText name = (EditText) findViewById(R.id.name_id);
+        final EditText price = (EditText) findViewById(R.id.price_id);
+        final Button addButton = (Button) findViewById(R.id.add_button_id);
+        final ListView items = (ListView) findViewById(R.id.list);
+        final ItemsAdapter adapter = new ItemsAdapter();
+        items.setAdapter(adapter);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.add(new Item(name.getText().toString(), Integer.parseInt(price.getText().toString())));
+            }
+        });
+    }
+
+    private class ItemsAdapter extends ArrayAdapter<Item> implements ListAdapter {
+        public ItemsAdapter() {
+            super(MainActivity.this, R.layout.list_item);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final View view = getLayoutInflater().inflate(R.layout.list_item, null);
+            final Item item = getItem(position);
+            ((TextView) view.findViewById(R.id.name)).setText(item.name);
+            ((TextView) view.findViewById(R.id.price)).setText(String.valueOf(item.price));
+            return view;
+        }
+    }
+}

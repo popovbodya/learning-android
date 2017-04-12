4170d1f026d3e3e76f76f0b425b0ea3945ebdb82
Bogdan Popov <popovbodya@gmail.com>
2017-04-12 15:12:13 +0300

    button handler added

--- first-steps/MyApplication/app/src/main/java/ru/popovbodya/myapplication/MyActivity.java
+++ first-steps/MyApplication/app/src/main/java/ru/popovbodya/myapplication/MyActivity.java
@@ -2,12 +2,32 @@ package ru.popovbodya.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
+
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(R.string.hot_welcome);
+            }
+        });
+    }
+
+    public void sayHello(View view) {
+        textView.setText(R.string.new_hello);
     }
 }

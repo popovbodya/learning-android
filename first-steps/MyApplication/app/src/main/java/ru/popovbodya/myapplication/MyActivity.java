43952520e82e963497a32c6c1234f2d1dfdd9eef
Bogdan Popov <popovbodya@gmail.com>
2017-04-12 23:01:28 +0300

    some new features added

--- first-steps/MyApplication/app/src/main/java/ru/popovbodya/myapplication/MyActivity.java
+++ first-steps/MyApplication/app/src/main/java/ru/popovbodya/myapplication/MyActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
 
     private TextView textView;
     private Button button;
+    private ImageButton kittyButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,25 @@ public class MyActivity extends Activity {
                 textView.setText(R.string.hot_welcome);
             }
         });
+
+        kittyButton = (ImageButton) findViewById(R.id.imageButton);
+        kittyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setVisible(v);
+            }
+        });
     }
 
     public void sayHello(View view) {
         textView.setText(R.string.new_hello);
+    }
+
+    private void setVisible(View view) {
+        if (textView.getVisibility() == View.VISIBLE) {
+            textView.setVisibility(View.INVISIBLE);
+        } else {
+            textView.setVisibility(View.VISIBLE);
+        }
     }
 }

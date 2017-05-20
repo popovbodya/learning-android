a5a059d3e2ae758b7aa08aa25e243ded181ad7de
Moonlight <bogdan.popov@mail.ru>
2017-05-20 20:11:39 +0300

    d

--- projects/current-works-at-classes/CurrentWork/app/src/main/java/bodya/sbt/ru/currentwork/MyApplication.java
+++ projects/current-works-at-classes/CurrentWork/app/src/main/java/bodya/sbt/ru/currentwork/MyApplication.java
@@ -3,51 +3,21 @@ package bodya.sbt.ru.currentwork;
 
 import android.app.Application;
 
-import java.util.ArrayList;
-import java.util.List;
+import bodya.sbt.ru.currentwork.interfaces.AnimalsStorageProvider;
 
-public class MyApplication extends Application {
+public class MyApplication extends Application implements AnimalsStorageProvider {
 
-    private List<Animal> animalList;
-    private final List<OnContentChangeListener> onContentChangeListeners;
-
-    public MyApplication() {
-        onContentChangeListeners = new ArrayList<>();
-    }
+    private AnimalStorage animalsStorage;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        animalList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            int age = (int) (Math.random() * 15) + 1;
-            int height = (int) (Math.random() * 50) + 1;
-            int weight = (int) (Math.random() * 50) + 1;
-            animalList.add(new Animal("Animal " + i, age, height, weight));
-        }
+        animalsStorage = new AnimalStorage();
     }
 
-    public List<Animal> getAnimalList() {
-        return new ArrayList<>(animalList);
+    @Override
+    public AnimalStorage getAnimalsStorage() {
+        return animalsStorage;
     }
 
-    public void addOnContentChangeListener(OnContentChangeListener listener) {
-        onContentChangeListeners.add(listener);
-    }
-
-    public void addAnimal(Animal animal) {
-        animalList.add(animal);
-        for (OnContentChangeListener listener : onContentChangeListeners) {
-            listener.onAnimalAdded(animal);
-        }
-    }
-
-    public void removeOnContentChangeListener(OnContentChangeListener listener) {
-        onContentChangeListeners.remove(listener);
-    }
-
-    public interface OnContentChangeListener {
-        void onAnimalAdded(Animal animal);
-    }
 }
